d6fa1bb456102783fe4b30bf2babce66d281bdb5
kauamscalzer <scalzerkauam@gmail.com>
2024-12-21 18:01:19 -0300

    chore: change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,16 @@ import type { Config } from 'jest'
 
 const config: Config = {
 	roots: ['<rootDir>/src/'],
-	collectCoverageFrom: ['<rootDir>/src/'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 	clearMocks: true,
 	collectCoverage: true,
 	coverageDirectory: 'coverage',
-	coverageProvider: 'v8'
+	coverageProvider: 'v8',
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.ts?$': 'ts-jest'
+	}
 }
 
 export default config
