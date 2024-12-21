472dfc729922a7e10f6dd05cf27d8a4c88cd2b0e
kauamscalzer <scalzerkauam@gmail.com>
2024-12-21 18:37:20 -0300

    refactor: move sut creation to a function

--- src/signup-controller.spec.ts
+++ src/signup-controller.spec.ts
@@ -2,15 +2,26 @@ import { MissingParamError } from './errors'
 import { badRequest } from './helpers'
 import { SignupController } from './signup-controller'
 
+type SutTypes = {
+	sut: SignupController
+}
+
+const makeSut = (): SutTypes => {
+	const sut = new SignupController()
+	return {
+		sut
+	}
+}
+
 describe('Signup Controller', () => {
 	test('Should return 400 if no name is provided', async () => {
-		const sut = new SignupController()
+		const { sut } = makeSut()
 		const result = await sut.handle({})
 		expect(result).toEqual(badRequest(new MissingParamError('name')))
 	})
 
 	test('Should return 400 if no email is provided', async () => {
-		const sut = new SignupController()
+		const { sut } = makeSut()
 		const result = await sut.handle({
 			name: 'any_name'
 		})
@@ -18,7 +29,7 @@ describe('Signup Controller', () => {
 	})
 
 	test('Should return 400 if no password is provided', async () => {
-		const sut = new SignupController()
+		const { sut } = makeSut()
 		const result = await sut.handle({
 			name: 'any_name',
 			email: 'any_email@mail.com'
