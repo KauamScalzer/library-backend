5a69425ee8af12e109fe5998d61d5d068b481e69
kauamscalzer <scalzerkauam@gmail.com>
2024-12-22 20:09:01 -0300

    feat: ensure DbAddUser call Encrypter with correct password

--- src/data/usecases/DbAddUser.ts
+++ src/data/usecases/DbAddUser.ts
@@ -1,13 +1,17 @@
 import type { IAddUser } from '../../domain/usecases'
-import type { ICheckUserByEmailRepository } from '../protocols'
+import type { ICheckUserByEmailRepository, IEncrypter } from '../protocols'
 
 export class DbAddUser implements IAddUser {
-	constructor(private readonly checkUserByEmailRepository: ICheckUserByEmailRepository) {}
+	constructor(
+		private readonly checkUserByEmailRepository: ICheckUserByEmailRepository,
+		private readonly encrypter: IEncrypter
+	) {}
 	async add(params: IAddUser.Params): Promise<IAddUser.Result> {
 		const user = await this.checkUserByEmailRepository.check(params.email)
 		if (user) {
 			return true
 		}
+		await this.encrypter.encrypt(params.password)
 		return
 	}
 }
